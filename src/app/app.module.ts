a1d278085eeb86f80e20af5686c5b736830e0f7e
rohan.shinde <akshudarakhe2001@gmail.com>
2023-04-29 14:53:22 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EducationComponent } from './education/education.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EducationComponent,
+    WorkExperienceComponent,
+    SkillsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
