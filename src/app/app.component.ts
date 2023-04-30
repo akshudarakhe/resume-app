d56234eb634a4d5fe166a3a0879eac0cd8656b54
akshudarakhe.com <akshudarakhe2001@gmail.com>
2023-05-01 01:23:02 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  jobTitle = 'Front End Developer';
+  about = 'I am a recent BCS graduate with a passion for front-end development. I have experience with Angular, CSS, Django, and Node.js, and I am also knowledgeable in the field of cybersecurity with a global certification in Certified Ethical Hacking. I am seeking a challenging role as a front-end developer to further develop my skills and contribute to innovative and creative projects.';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    EducationComponent,
+    SkillsComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
